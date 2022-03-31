b011dbb3aeb56f17738161b9c2b360be651c651b
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-03-31 09:17:03 +0300

    Atliktas pirmas pavyzdys tik su kintamaisiais

---
+++ kintamieji/script.js
@@ -0,0 +1,21 @@
+// Pagrindinė (main) funkcija
+window.onload = function() {
+    // kintamieji
+    let pirmoAlga;
+    let antroAlga;
+
+    let bendraAlga;
+
+
+    pirmoAlga = 1200;
+    antroAlga = 800;
+
+    bendraAlga = pirmoAlga + antroAlga;
+
+    console.log("Abiejų bendra alga yra: " + bendraAlga);
+
+
+    // Papildomas "sunkesnis" išvedimas:
+    console.log("Sudėjus pirmojo algą, kuri yra " + pirmoAlga + " su antrojo alga, kuri yra " + antroAlga + " gauname rezultatą: " + bendraAlga)
+
+}
